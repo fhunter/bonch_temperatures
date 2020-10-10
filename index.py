c755a1129010a2fd781709f7f2d505edde39a973
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2020-10-10 20:19:07 +0300

    Added grouped temperature

--- index.py
+++ index.py
@@ -33,20 +33,40 @@ def main_grouped(grouped, period):
 def graph(name,grouped,period):
     " Graph endpoint, returns generated graph "
     pd = "-1"+period
-    test = rrdtool.graphv("-", "--start", pd, "-w 800", "--title=Температуры %s" % name,
-                          "-u 60", "-l 15",  
-                          "DEF:cpu_temp=rrd/%s.rrd:ds0:MAX" % name,
-                          "DEF:hdd_temp=rrd/%s.rrd:ds1:MAX" % name,
-                          "LINE1:cpu_temp#0000FF:Процессор",
-                          "LINE2:hdd_temp#FF0000:Диск\\j",
-                          "CDEF:unavailable=hdd_temp,UN,INF,0,IF",
-                          "AREA:unavailable#f0f0f0",
-                          "GPRINT:cpu_temp:MAX:Максимум\\: процессор\\: %3.0lf °C",
-                          "GPRINT:hdd_temp:MAX:жёсткий диск\\: %3.0lf °C\\j",
-                          "GPRINT:cpu_temp:MAX:Текущий\\: процессор\\: %3.0lf °C",
-                          "GPRINT:hdd_temp:MAX:жёсткий диск\\: %3.0lf °C\\j"
-                         )
-
+    if grouped == "i":
+        test = rrdtool.graphv("-", "--start", pd, "-w 800", "--title=Температуры %s" % name,
+                              "-u 60", "-l 15",  
+                              "DEF:cpu_temp=rrd/%s.rrd:ds0:MAX" % name,
+                              "DEF:hdd_temp=rrd/%s.rrd:ds1:MAX" % name,
+                              "LINE1:cpu_temp#0000FF:Процессор",
+                              "LINE2:hdd_temp#FF0000:Диск\\j",
+                              "CDEF:unavailable=hdd_temp,UN,INF,0,IF",
+                              "AREA:unavailable#f0f0f0",
+                              "GPRINT:cpu_temp:MAX:Максимум\\: процессор\\: %3.0lf °C",
+                              "GPRINT:hdd_temp:MAX:жёсткий диск\\: %3.0lf °C\\j",
+                              "GPRINT:cpu_temp:MAX:Текущий\\: процессор\\: %3.0lf °C",
+                              "GPRINT:hdd_temp:MAX:жёсткий диск\\: %3.0lf °C\\j"
+                             )
+    else:
+        names = glob.glob("./rrd/%s*.rrd" % name)
+        names.sort()
+        new_names = []
+        for i in names:
+            new_names.append(i.replace('./rrd/', '').replace('.rrd', ''))
+	arguments = ("-", "--start", pd, "-w 800", "--title=Температуры %s" % name, "-u 60", "-l 15")
+	j=1
+	for i in new_names:
+	    new_arguments = ( "DEF:cpu_temp%d=rrd/%s.rrd:ds0:MAX" % (j,i),
+                              "DEF:hdd_temp%d=rrd/%s.rrd:ds1:MAX" % (j,i),
+                              "LINE%d:cpu_temp%d#0000FF:Процессор" % (j,j),
+                              "LINE%d:hdd_temp%d#FF0000:Диск\\j" % (j*2+1,j),
+                              "GPRINT:cpu_temp%d:MAX:Максимум\\: процессор\\: %%3.0lf °C" %(j),
+                              "GPRINT:hdd_temp%d:MAX:жёсткий диск\\: %%3.0lf °C\\j" % (j),
+                              "GPRINT:cpu_temp%d:MAX:Текущий\\: процессор\\: %%3.0lf °C" % (j),
+                              "GPRINT:hdd_temp%d:MAX:жёсткий диск\\: %%3.0lf °C\\j" %(j))
+            j=j+1
+	    arguments = arguments + new_arguments
+        test = rrdtool.graphv(*arguments)
     response.set_header('Content-type', 'image/png')
     return str(test['image'])
 
